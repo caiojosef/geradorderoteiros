5ef61c2bc0c88f3f7ee3b5f8b374bb858a120077
Caio Josef <caioojoseff@gmail.com>
2025-12-11 15:13:18 -0300

    alterações no site

--- app/config/database.php
+++ app/config/database.php
@@ -1,19 +1,28 @@
 <?php
 
-class Database {
+class Database
+{
     private $host = "localhost";
     private $user = "caiojo79_admuser";
     private $pass = "261456Caio*josef";
-    private $db   = "caiojo79_geradorderoteiros";
+    private $db = "caiojo79_geradorderoteiros";
 
     public $conn;
 
     public function __construct()
     {
-        $this->conn = new mysqli($this->host, $this->user, $this->pass, $this->db);
+        $this->conn = new mysqli(
+            $this->host,
+            $this->user,
+            $this->pass,
+            $this->db
+        );
 
         if ($this->conn->connect_error) {
             die("Erro ao conectar ao MySQL: " . $this->conn->connect_error);
         }
+
+        // força charset UTF-8 (importante!)
+        $this->conn->set_charset("utf8mb4");
     }
 }
--- app/core/Controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Controller {
-    public function view($view, $data = [])
-    {
-        extract($data);
-        require "../app/views/{$view}.php";
-    }
-}
